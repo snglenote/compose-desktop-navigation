86b28729f53ca1aad973fab54eac207b6c4b2dae
Calvine Akoonyatse <cakoonyatse@gmail.com>
2023-04-05 23:40:42 +0200

    Update Main.kt

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,25 +1,71 @@
-import androidx.compose.material.MaterialTheme
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+
+import screens.homeScreen
+import screens.settingScreen
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+
+    val screens = Screen.values().toList()
+    val navController by rememberNavController(Screen.HomeScreen.name)
+    val currentScreen by remember {
+        navController.currentScreen
+    }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Surface(
+            modifier = Modifier.background(color = MaterialTheme.colors.background)
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                NavigationRail(
+                    modifier = Modifier.align(Alignment.CenterStart).fillMaxHeight()
+                ) {
+                    screens.forEach {
+                        NavigationRailItem(
+                            selected = currentScreen == it.name,
+                            icon = {
+                                Icon(
+                                    imageVector = it.icon,
+                                    contentDescription = it.label
+                                )
+                            },
+                            label = {
+                                Text(it.label)
+                            },
+                            alwaysShowLabel = false,
+                            onClick = {
+                                navController.navigate(it.name)
+                            }
+                        )
+                    }
+                }
+
+                Box(
+                    modifier = Modifier.fillMaxHeight()
+                ) {
+                    CustomNavigationHost(navController = navController)
+                }
+            }
         }
     }
 }
@@ -28,4 +74,41 @@ fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
     }
+}
+
+
+/**
+ * Screens
+ */
+enum class Screen(
+    val label: String,
+    val icon: ImageVector
+) {
+    HomeScreen(
+        label = "Home",
+        icon = Icons.Filled.Home
+    ),
+
+    SettingsScreen(
+        label = "Settings",
+        icon = Icons.Filled.Settings
+    ),
+
+}
+
+
+@Composable
+fun CustomNavigationHost(
+    navController: NavController
+) {
+    NavigationHost(navController) {
+        composable(Screen.HomeScreen.name) {
+            homeScreen(navController)
+        }
+
+        composable(Screen.SettingsScreen.name) {
+            settingScreen(navController)
+        }
+
+    }.build()
 }
