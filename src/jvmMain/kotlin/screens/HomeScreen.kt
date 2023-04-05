316a31dfc5365bd0a82a70bbbf3ad512c91a630b
Calvine Akoonyatse <cakoonyatse@gmail.com>
2023-04-05 23:40:46 +0200

    Create HomeScreen.kt

---
+++ src/jvmMain/kotlin/screens/HomeScreen.kt
@@ -0,0 +1,31 @@
+package screens
+
+import NavController
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun homeScreen(
+    navController: NavController
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(navController.currentScreen.value)
+        Button(
+            onClick = {
+                navController.navigate(Screen.SettingsScreen.name)
+            }) {
+            Text("Navigate to Settings")
+        }
+    }
+}
